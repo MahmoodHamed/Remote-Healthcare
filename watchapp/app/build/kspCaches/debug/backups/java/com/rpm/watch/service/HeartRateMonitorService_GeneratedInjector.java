ac524a63302988b679fa7c0f4ac25f9fd116f49e
Mahmood Hamed <mahmood@example.com>
2026-05-09 19:09:16 +0300

    fix bugs

---
+++ watchapp/app/build/kspCaches/debug/backups/java/com/rpm/watch/service/HeartRateMonitorService_GeneratedInjector.java
@@ -0,0 +1,17 @@
+package com.rpm.watch.service;
+
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.ServiceComponent;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.hilt.internal.GeneratedEntryPoint;
+import javax.annotation.processing.Generated;
+
+@OriginatingElement(
+    topLevelClass = HeartRateMonitorService.class
+)
+@GeneratedEntryPoint
+@InstallIn(ServiceComponent.class)
+@Generated("dagger.hilt.android.processor.internal.androidentrypoint.InjectorEntryPointGenerator")
+public interface HeartRateMonitorService_GeneratedInjector {
+  void injectHeartRateMonitorService(HeartRateMonitorService heartRateMonitorService);
+}
