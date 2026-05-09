37b03c0c88a6b1365b3108165ec1a6d84382abe3
Mahmood Hamed <mahmood@example.com>
2026-05-09 16:56:49 +0300

    fix bugs

--- backend/src/RPM.Domain/obj/Debug/net10.0/RPM.Domain.AssemblyInfo.cs
+++ backend/src/RPM.Domain/obj/Debug/net10.0/RPM.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPM.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec187ada685eea32bf1a4fd85d6ac0814f520838")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cb39f7df030ca3d9cae7f66094e1db16e1bf7d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPM.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPM.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
